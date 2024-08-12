af8dc89f8e84c6262c985c080ae8d9842c975e0c
m.mohamadi <m.mohamadi490@gmail.com>
2024-08-12 20:05:35 +0330

    create setting class in config file with pydantic

---
+++ app/core/config.py
@@ -0,0 +1,13 @@
+from pydantic import BaseSettings
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+class Settings(BaseSettings):
+    database_url: str = os.getenv("DATABASE_URL")
+    secret_key: str = os.getenv("SECRET_KEY")
+    debug: bool = os.getenv("DEBUG")
+
+
+settings = Settings()
