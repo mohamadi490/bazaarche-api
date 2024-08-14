48b477867122b915290e72e15ede4379098ea132
m.mohamadi <m.mohamadi490@gmail.com>
2024-08-14 18:39:14 +0330

    create model for user, role and permissions in user file

---
+++ app/db/models/user.py
@@ -0,0 +1,49 @@
+from db.base import Base
+from sqlalchemy import Column, Table, ForeignKey, DateTime, Integer, String, Boolean
+from sqlalchemy.orm import relationship
+from datetime import datetime
+
+# Association table for many-to-many relationship between Role and Permission
+role_permission = Table('role_permission', Base.metadata,
+    Column('role_id', Integer, ForeignKey('roles.id')),
+    Column('permission_id', Integer, ForeignKey('permissions.id'))
+)
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    phone_number = Column(String)
+    role_id = Column(Integer, ForeignKey('roles.id'))
+    password = Column(String)
+    is_active = Column(Boolean, default=True)
+    created_at = Column(DateTime, default=datetime.now)
+    updated_at = Column(DateTime, default=datetime.now)
+    deleted_at = Column(DateTime, nullable=True)
+
+class Role(Base):
+    __tablename__ = 'roles'
+    
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
+    tag = Column(String)
+    created_at = Column(DateTime, default=datetime.now)
+    updated_at = Column(DateTime, default=datetime.now)
+    deleted_at = Column(DateTime, nullable=True)
+    
+    permissions = relationship("Permission", secondary=role_permission, back_populates="roles")
+    
+class Permission(Base):
+    __tablename__ = 'permissions'
+    
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
+    created_at = Column(DateTime, default=datetime.now)
+    updated_at = Column(DateTime, default=datetime.now)
+    deleted_at = Column(DateTime, nullable=True)
+    
+    roles = relationship("Role", secondary=role_permission, back_populates="permissions")
