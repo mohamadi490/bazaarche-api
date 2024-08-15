cfa9df0616083539dbe26978b47b710d4baaa82a
m.mohamadi <m.mohamadi490@gmail.com>
2024-08-15 13:15:32 +0330

    add algorithm and expire minute in settings for jwt

--- app/core/config.py
+++ app/core/config.py
@@ -7,7 +7,8 @@ load_dotenv()
 class Settings(BaseSettings):
     database_url: str = os.getenv("DATABASE_URL")
     secret_key: str = os.getenv("SECRET_KEY")
+    algorithm: str = os.getenv("ALGORITHM")
     debug: bool = os.getenv("DEBUG")
-
+    access_token_expire_minutes: int = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
 
 settings = Settings()
