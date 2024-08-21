af841c67d91e4e748c92cc387daa6c146d48780f
m.mohamadi <m.mohamadi490@gmail.com>
2024-08-21 17:35:29 +0330

    add user_id to product model

--- app/db/models/product.py
+++ app/db/models/product.py
@@ -24,6 +24,7 @@ class Product(Base):
     __tablename__ = 'products'
     
     id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
     name = Column(String, nullable=False)
     slug = Column(String, unique=True, nullable=False)
     type = Column(Enum(ProductType), nullable=False)
