2a3ca5ab55848fb3efbd75d3322d24e7ffe2f198
m.mohamadi <m.mohamadi490@gmail.com>
2024-08-26 15:58:35 +0330

    add product router to main

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ from routers.v1.users import router as users_router
 from routers.v1.auth import router as auth_router
 from routers.v1.roles import role_router
 from routers.v1.category import category_router
+from routers.v1.product import product_router
 
 Base.metadata.create_all(bind=engine)
 
@@ -14,3 +15,4 @@ app.include_router(auth_router, prefix="/api/v1")
 app.include_router(users_router, prefix="/api/v1")
 app.include_router(role_router, prefix="/api/v1")
 app.include_router(category_router, prefix="/api/v1")
+app.include_router(product_router, prefix="/api/v1")
