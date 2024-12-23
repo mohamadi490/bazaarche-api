9ee422fad318b7d23efcabb67110336a32b951ab
m.mohamadi <m.mohamadi490@gmail.com>
2024-12-23 16:39:58 +0330

    create transaction_enums

---
+++ app/models/enums/transaction_enums.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+
+class TransactionType(Enum):
+    order = "order"
+    wallet_deposit = "wallet_deposit"
+    wallet_withdraw = "wallet_withdraw"
+
+
+class TransactionStatus(Enum):
+    Success = "success"
+    Failed = "failed"
+    Pending = "pending"
